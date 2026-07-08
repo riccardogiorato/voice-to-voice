c7e5531d73f7018e6d94ce608cedbda4a96e22b5
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2026-07-08 18:27:46 +0200

    feat: add permissions policy header to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "microphone=(self)",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
